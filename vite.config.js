8647e81242acaab4e023c2c0fb59e1b8df034a98
mcyepesa0609-crypto <mcyepesa0609@gmail.com>
2026-02-02 23:46:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SIGMMA-MAR/", // <-- IMPORTANTÍSIMO (incluye slash final)
+  base: "/SIGMMA-MAR/",
 });
